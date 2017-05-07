e8e1b607f56b0758e5df28b4988d325c5506e67b
John <johnidfet@gmail.com>
2017-05-07 23:42:16 +0800

    add license header

--- include/Avalon/util/logger/ostream_adapter.hpp
+++ include/Avalon/util/logger/ostream_adapter.hpp
@@ -1,3 +1,23 @@
+/**
+ *Copyright (C) 2017  Chia-Hua Chang
+ *
+ *This file is part of Avalon.
+ *
+ *Avalon is free software: you can redistribute it and/or modify
+ *it under the terms of the GNU General Public License as published by
+ *the Free Software Foundation, either version 3 of the License, or
+ *(at your option) any later version.
+ *
+ *Avalon is distributed in the hope that it will be useful,
+ *but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *GNU General Public License for more details.
+ *
+ *You should have received a copy of the GNU General Public License
+ *along with Avalon.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 
 #pragma once
 #include <iostream>
@@ -10,7 +30,6 @@
 #include <Avalon/type_traits/pointer.hpp>
 #include <Avalon/const.hpp>
 namespace avalon{ namespace util{ namespace logger{
-// TODO wchar support
 
 template<class OSTREAM_PTR>
 class OstreamAdapter
